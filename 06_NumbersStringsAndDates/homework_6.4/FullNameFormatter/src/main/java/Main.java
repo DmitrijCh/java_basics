016f82dfe2b03d0800c8b831417b3799e100a215
Dmitriy <dimach3688@gmail.com>
2022-03-31 17:43:17 +0400

    finished

--- 06_NumbersStringsAndDates/homework_6.4/FullNameFormatter/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.4/FullNameFormatter/src/main/java/Main.java
@@ -1,40 +1,38 @@
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
 
-      int count = 0;
-      int spaceIndex = input.indexOf(' ');
-      int spaceLastIndex = input.lastIndexOf(' ');
-      if(input.length() != 0)
-      {
-        count++;
-        for(int i = 0; i < input.length(); i++){
-          if(input.charAt(i) == ' '){
-            count++;
-          }
+            int count = 0;
+            int spaceIndex = input.indexOf(' ');
+            int spaceLastIndex = input.lastIndexOf(' ');
+            if (input.length() != 0) {
+                count++;
+                for (int i = 0; i < input.length(); i++) {
+                    if (input.charAt(i) == ' ') {
+                        count++;
+                    }
+                }
+            }
+            if (count != 3) {
+                System.out.println("Введенная строка не является ФИО");
+                break;
+            }
+
+            String firstName = input.substring(0, spaceIndex);
+            String lastName = input.substring(spaceIndex + 1, spaceLastIndex);
+            String otherName = input.substring(spaceLastIndex + 1);
+
+            System.out.println("Фамилия: " + firstName);
+            System.out.println("Имя: " + lastName);
+            System.out.println("Отчество: " + otherName);
+
         }
-      }
-      if(count != 3){
-        System.out.println("Введенная строка не является ФИО");
-        break;
-      }
-
-      String firstName = input.substring(0, spaceIndex);
-      String lastName = input.substring(spaceIndex + 1, spaceLastIndex);
-      String otherName = input.substring(spaceLastIndex + 1);
-
-      System.out.println("Фамилия: " + firstName);
-      System.out.println("Имя: " + lastName);
-      System.out.println("Отчество: " + otherName);
-
     }
-  }
-
 }
