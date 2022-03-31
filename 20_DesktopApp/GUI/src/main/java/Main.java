13fc961df9b1b74f2651833de0694d44e9e39dd6
Dmitriy <dimach3688@gmail.com>
2022-03-31 17:33:56 +0400

    finished

--- 20_DesktopApp/GUI/src/main/java/Main.java
+++ 20_DesktopApp/GUI/src/main/java/Main.java
@@ -1,8 +1,7 @@
 import java.util.List;
 
 public class Main {
-
-  public static void main(String[] args) {
-    new Form1(List.of("", "", ""));
-  }
+    public static void main(String[] args) {
+        new Form1(List.of("", "", ""));
+    }
 }
